bc74480039b4280c3052566a6feba77a168f646e
Vojtěch Dejnožka <57211359+VojtechDejnozka@users.noreply.github.com>
2020-03-19 21:48:46 +0100

    Add files via upload

---
+++ Rock-Paper-Scissors/RockPaperScissors/RockPaperScissors/Service/RpsLogic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RockPaperScissors.Model;
+
+namespace RockPaperScissors.Service
+{
+    public class RpsLogic
+    {
+        private readonly Random random;
+
+        public RpsLogic(Random random)
+        {
+            this.random = random;
+        }
+
+        public Rps userData { get; set; }
+        
+        public void GameRound(RpsMode userChoice)
+        {           
+            RpsMode aiChoice = (RpsMode)random.Next(1, 4);
+
+
+            if (aiChoice == RpsMode.Rock && userChoice == RpsMode.Paper ||
+                aiChoice == RpsMode.Paper && userChoice == RpsMode.Scissors ||
+                aiChoice == RpsMode.Scissors && userChoice == RpsMode.Rock)
+            {
+                userData.WinCounter++;
+                userData.IsWinner = true;
+            }
+
+            else if (aiChoice == userChoice)
+            {
+                userData.DrawCounter++;
+            }
+            else userData.LossCounter++;
+
+            
+        }
+    }
+}
